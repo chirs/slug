3e641c6779f1352b7099fc56bf977bb437dd6594
Chris Edgemon <chrisedgemon@gmail.com>
2013-03-18 12:57:38 -0400

    Add basic error-handling for input.js

--- input.js
+++ input.js
@@ -3,10 +3,17 @@ var scheme = require('./scheme.js');
 var repl = function() {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
+  process.stdout.write("scheme>> ");
   process.stdin.on('data', function (chunk) {
     var expr = scheme.parse(chunk);
-    returnValue = String(scheme.sEval(expr));
-    process.stdout.write(returnValue + '\n');
+    try {
+      returnValue = String(scheme.sEval(expr));
+      process.stdout.write(returnValue + '\n');
+    } catch(err) {
+      process.stdout.write("Whoops! We had an error.\n");
+    }
+    process.stdout.write("scheme>> ");
+
   });
 };
 
